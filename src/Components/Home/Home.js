ce8bd9a451854af6e3b40cb5034f22b4f8ccea32
Vyshnav-7338 <vyshnavprasannakumar200@gmail.com>
2023-09-13 12:24:43 +0530

    initial commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,18 @@
+import List from '../List/List'
+import React, { useState } from 'react'
+import { data } from '../../Static Data/Data'
+function Home() {
+    const [people,setPeople]=useState(data)
+    
+  return (
+   <main>
+    <section className='container'>
+    <h3>{people.length} Birthday Today</h3>
+    <List people={people}/>
+    <button onClick={()=>setPeople([])}>Clear All</button>
+    </section>
+   </main>
+  )
+}
+
+export default Home
---
+++ src/Static Data/Data.js
@@ -0,0 +1,37 @@
+import anju from '../Image/anju.jpeg'
+import arjun from '../Image/arjun.jpg'
+import kelwin from '../Image/kelwin.jpg'
+import sandra from '../Image/sandra.jpeg'
+import vyshnav from '../Image/Vyshnav.jpg'
+export const data=[
+    {
+        id:1,
+        name:'Anjitha',
+        age:20,
+        image:anju
+    },
+    {
+        id:2,
+        name:'Arjun',
+        age:20,
+        image:arjun
+    },
+    {
+        id:3,
+        name:'Kelwin',
+        age:22,
+        image:kelwin
+    },
+    {
+        id:4,
+        name:'sandra',
+        age:22,
+        image:sandra
+    },
+    {
+        id:5,
+        name:'vyshnav',
+        age:23,
+        image:vyshnav
+    },
+]
